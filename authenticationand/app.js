d0c7eff37cd534a36980931647778c5aefe4582f
Abdullah Javaid <akhaan686@gmail.com>
2024-12-31 22:45:27 +0500

    changes

--- authenticationand/app.js
+++ authenticationand/app.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const cookieParser = require('cookie-parser')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser');
 
 
-app.use(cookieParser())
+app.set('view engine' , 'ejs')
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
+app.use(express.static(path.join(__dirname , 'public')))
+app.use(cookieParser());
+
+
 
 app.get('/' , (req, res) =>{
    res.send("done")
 })
-app.get('/read' , (req, res) =>{
-   res.send("done")
-})
+
 
 
 
